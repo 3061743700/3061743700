7ced901d621552280f21eae4fb6906e4517ac619
lisi <lisi@qq.com>
2022-10-08 13:29:36 +0800

    修改

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("11111111111111111111");
+        System.out.println("1+1=2");
+    }
+}
