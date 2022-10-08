aa6da1beb358bd7c402bf1f833191f8fbb2dbe59
lisi <lisi@qq.com>
2022-10-08 13:31:08 +0800

    循环

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,9 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("11111111111111111111");
         System.out.println("1+1=2");
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
     }
 }
